9fcd7a53b4f85518e37027253ab59a574a522955
onikiienko <onikiienko@gmail.com>
2013-12-19 19:08:27 +0200

    w

--- tests/source/screenshoter.py
+++ tests/source/screenshoter.py
@@ -45,7 +45,6 @@ def compare(dali, file1, file2, screenName):
         try_to_delete_file(result_file)
     else:
         print 'http://functest.maps2.test/screens/' + screenName + '.png';
-        print 'ПЕРВЫЙ ФАЙЛ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!' + file1;
         #try_to_delete_file(file1)
         #try_to_delete_file(file2)
 
