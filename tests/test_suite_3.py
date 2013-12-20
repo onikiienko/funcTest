fc9e37cf7716ed4c89c0c4afd9fe54b72a24082e
Gaidamak <gaidamak.pasha@gmail.com>
2013-12-20 16:28:56 +0200

    Fix test_suite_3.py

--- tests/test_suite_3.py
+++ tests/test_suite_3.py
@@ -21,7 +21,7 @@ class Suite(BaseSuite):
 # Hover your mouse over the marker (RR: Change marker icons, hint)
     def test3_2(self):
         def callback(driver):
-            marker = driver.find_element_by_class_name('dg-marker')
+            marker = driver.find_element_by_class_name('leaflet-marker-icon')
             ActionChains(driver).move_to_element(marker).perform()
             wait_for_page_load(driver)
 
@@ -31,7 +31,7 @@ class Suite(BaseSuite):
 # Grab a marker and drag it to another street (RR: marker moved to the expected street hint (provided that the tests will be mouse over the marker)
     def test3_3(self):
         def callback(driver):
-            marker = driver.find_element_by_class_name('dg-marker')
+            marker = driver.find_element_by_class_name('leaflet-marker-icon')
             ActionChains(driver).drag_and_drop_by_offset(marker, 100, 100).perform()
             wait_for_page_load(driver)
 
@@ -42,7 +42,7 @@ class Suite(BaseSuite):
 # Click on the marker (RR: smenka marker icons, there is no hint)
     def test3_4(self):
         def callback(driver):
-            marker = driver.find_element_by_class_name('dg-marker')
+            marker = driver.find_element_by_class_name('leaflet-marker-icon')
             ActionChains(driver).click(marker).perform()
             wait_for_page_load(driver)
 
