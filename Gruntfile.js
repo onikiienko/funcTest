ad40c0130f72eea16a38ba8098612fb04af69c1a
Bogdan Onikiienko <onikiienko@gmail.com>
2014-01-15 20:55:26 +0200

    trying to interact project with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ grunt.initConfig({
             options: {                      // Options
                 stdout: true
             },
-            command: 'python tests_launcher'
+            command: 'python tests_launcher.py'
         }
     }
 });
