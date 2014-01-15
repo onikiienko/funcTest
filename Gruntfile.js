660731966a9fa7deb57d52b366a48713f52e4789
Bogdan Onikiienko <onikiienko@gmail.com>
2014-01-15 20:00:22 +0200

    trying to interact project with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+grunt.initConfig({
+    shell: {
+        pythonServer: {
+            options: {
+                stdout: true
+            },
+            command: 'python tests_launcher.py'
+        }
+    }
+});
+
+grunt.loadNpmTasks('grunt-shell');
