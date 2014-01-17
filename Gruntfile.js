9aa19418ee5a5b3d8dd7024ce4832139bbf1d4da
Bogdan Onikiienko <onikiienko@gmail.com>
2014-01-17 13:51:23 +0200

    made functional for filling config from local config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,4 @@
-var config = require('./config.json');
-
-
-// Здесь получаем нужное свойство
-
-function makePaterns(){
-	var patterns = [];
-	for(var key in config){
-		patterns.push({match: key, replacement: config[key]})
-	}
-	return patterns;
-}
+var config = {};
 
 module.exports = function(grunt) {
 	grunt.initConfig({
@@ -37,3 +26,28 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-shell');
 	grunt.registerTask('default', ['replace']);
 };
+
+
+function makePaterns(){
+	fillConfig();
+	var patterns = [];
+	for(var key in config){
+		patterns.push({match: key, replacement: config[key]})
+	}
+	return patterns;
+}
+
+function fillConfig() {
+		config = require('./config.json');
+		var fs = require('fs');
+
+		var exists = fs.existsSync('./config.local.json'); 
+		if (exists) {
+		    var localConfig = require('./config.local.json');
+			for(var key in localConfig){
+				config[key] = localConfig[key];
+			}
+		 } else {
+		    console.log("no local file was found");
+		 }
+}
