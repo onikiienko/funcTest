127c6b50c11d2a5a39cbfeed8020d5e6439462ae
Bogdan Onikiienko <onikiienko@gmail.com>
2014-03-13 17:01:34 +0200

    gitignore

--- build/testSuites/geoclicker.js
+++ build/testSuites/geoclicker.js
@@ -1,18 +1,22 @@
-var wd = require('selenium-webdriver');
+var wd = require('selenium-webdriver'),
+	map = require('../../helper');
 
-var testSuite = [];
-var test1 = function(driver) {
-	return wd.promise.Promise;
-}
+var testSuite = [
+	function test1(driver) {
+		map.waitForDownload(driver);
+		map.clickCenter(driver);
+		map.waitForDownload(driver);
+		driver.findElement({css: ".dg-zoom__in"}).click();
+		map.waitForDownload(driver);
+		return wd.promise.Promise;
+	},
+	function test2(driver) {
+		map.waitForDownload(driver);
+		return wd.promise.Promise;
+	}
+];
 
-var test2 = function(driver) {
-	return wd.promise.Promise;
-}
-
-testSuite.push(test1);
-testSuite.push(test2);
 module.exports.testSuite = testSuite;
-module.exports.testUrl = '@@functest/demos/@@skinColor/test/geoclicker.html';
-module.exports.publicUrl = '@@functest/demos/@@skinColor/public/geoclicker.html';
-//module.exports.testUrl = 'http://127.0.0.1:3001/demos/dark/public/wkt.html';
-//module.exports.publicUrl = 'http://127.0.0.1:3001/demos/dark/public/wkt.html';
+var demoName = 'geoclicker';
+module.exports.testUrl = '@@functest/demos/@@skinColor/public/' + demoName + '.html';
+module.exports.publicUrl = '@@functest/demos/@@skinColor/public/' + demoName + '.html';
