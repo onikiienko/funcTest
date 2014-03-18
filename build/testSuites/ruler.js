d51fef664513b4d6814699e14a7b84feeb816636
Gaidamak <gaidamak.pasha@gmail.com>
2014-03-18 16:35:04 +0200

    Updates

--- build/testSuites/ruler.js
+++ build/testSuites/ruler.js
@@ -1,22 +1,30 @@
 var wd = require('selenium-webdriver'),
-	map = require('../../helper');
+	helper = require('../../helper');
 
 var testSuite = [
-	function clickInHouse(driver) {
-		map.waitForDownload(driver);
-		map.clickCenter(driver);
-		map.waitForDownload(driver);
-		driver.findElement({css: ".dg-zoom__in"}).click();
-		map.waitForDownload(driver);
+	function enableRuler(driver) {
+		helper.waitForDownload(driver);
+		driver.executeScript("alert('Ruler')");
+		driver.findElement({class: ".dg-control-round_icon__ruler"}).click();
+		helper.waitForDownload(driver);
 		return wd.promise.Promise;
 	},
-	function clickInStreet(driver) {
-		map.waitForDownload(driver);
+	function markFirstPoint(driver) {
+		helper.waitForDownload(driver);
+		helper.clickCenter(driver);
+		helper.waitForDownload(driver);
 		return wd.promise.Promise;
-	}
+	},
+	function dragPoint(driver) {
+		helper.waitForDownload(driver);
+		driver.findElement(webdriver.By.xpath("//*[@id='map']/div[1]/div[2]/div[3]/div/div/div")).mouseDown();
+		helper.mouseMove();
+		return wd.promise.Promise;
+	},
+
 ];
 
 module.exports.testSuite = testSuite;
-var demoName = 'geoclicker';
+var demoName = 'ruler';
 module.exports.testUrl = '@@functest/demos/@@skinColor/public/' + demoName + '.html';
 module.exports.publicUrl = '@@functest/demos/@@skinColor/public/' + demoName + '.html';
